01f03948032645d3b397676d60c12b1de05028c5
Mohamed Elbana <88998148+Mo7amed-2lbana@users.noreply.github.com>
2023-07-22 15:05:38 +0300

    Ubdate Layout File

--- src/Componant/Layout/Layout.jsx
+++ src/Componant/Layout/Layout.jsx
@@ -1,60 +1,23 @@
 import React, { useEffect, useRef, useState } from "react";
 import Loading from "./../Loading/Loading";
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { incrementValue } from "../../Store/LoadingSlice";
 
 export default function Layout() {
   const dispatch = useDispatch();
 
-  const [products, setProducts] = useState(null);
+  // useEffect(() => {
+  //   dispatch(incrementValue());
+  // }, [!products]);
 
-  async function getData() {
-    const { data } = await axios.get(
-      "https://ecommerce.routemisr.com/api/v1/products"
-    );
-    setProducts(data.data);
-    console.log(data.data);
-  }
-
-  useEffect(() => {
-    getData();
-  });
-
-  useEffect(() => {
-    dispatch(incrementValue());
-  }, [!products]);
-
-  return (
-
-    <>
-      {products ?<>
-        <div className="container">
-
-          <div className="row">
-            {products.map((product)=>{
-              return <>
-              <div className="col-md-2">
-
-                  <div className="item">
-                    <h2>{product.title}</h2>
-                  </div>
-
-              </div>
-              
-              
-              </>
-            })}
-          </div>
-        </div>
-      
-      
-      
-      </>: (
-        <>
-          <Loading />
-        </>
-      )}
-    </>
-  );
+  return <>
+  
+  
+  
+  
+  
+  
+  
+  </>;
 }
